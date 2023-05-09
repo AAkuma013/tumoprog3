382af6059f8dbad570cc349ce3fe85d0ae662194
AAkuma013 <narek.madatyan@tumo.org>
2023-05-09 22:12:59 +0400

    upd

--- Lesson2/script.js
+++ Lesson2/script.js
@@ -1,26 +1,21 @@
 //import json5
 
 var obj = {
+  first_name: "Vardan",
 
-    "first_name": "Vardan",
-    
-    "last_name": "Hovsepyan",
-    
-    "age": 13,
-    
-    "tumo_student": false
-    
-    
-    
-    
-    }
+  last_name: "Hovsepyan",
 
-console.log(obj)
+  age: 13,
 
-let a = new obj()
+  tumo_student: false,
+
+};
+
+let myString = JSON.stringify(obj)
+prompt(myString)
+//let a = new obj()
 ///js_obj = '{x:1, y:2, z:3}'
-    
+
 //py_obj = json5.loads(js_obj)
-    
+
 //print(py_obj)
-    

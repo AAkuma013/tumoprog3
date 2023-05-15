cf0582985c3fdb2a29b116ff23f47c26aa57597c
AAkuma013 <narek.madatyan@tumo.org>
2023-05-15 12:07:42 +0400

    ee

--- lesson3/index.js
+++ lesson3/index.js
@@ -1,1 +1,58 @@
-console.log("Hello node");
+/*var fs = require('fs');
+
+function main(){
+
+var file = "hello.txt";
+
+fs.appendFileSync(file, "ehefheufheufheufheufheufheufheuhfeuhfeuh\n");
+
+}
+
+main(); */
+
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req, res){
+
+res.send("<h1>Hello world</h1>");
+
+});
+
+app.get("/name/:name", function(req, res){
+
+var name = req.params.name;
+
+res.send("<h1>Hello " + name +"</h1>");
+
+});
+
+app.get("/google", function(req, res){
+
+    res.redirect("https://google.com");
+    
+});
+
+app.get("/google/:search", function(req, res){
+    var search = req.params.search
+
+    res.redirect("https://google.com/search?q="+search);
+    
+});
+
+
+
+app.get("/*", function(req, res, err){
+
+    res.status(404).send('<h1>ERROR 404</h1>');
+    
+});
+
+
+
+app.listen(3000, function(){
+
+console.log("Example is running on port 3000");
+
+});
